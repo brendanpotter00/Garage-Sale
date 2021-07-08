55726f9aef522d4e4e8aadd24786750726e8c34a
bpotter-trinity <bpotter@trinity.edu>
2021-07-08 00:30:16 -0500

    working on user route, and finished login and register

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -20,23 +20,21 @@ router.post("/register", async (req, res) => {
     }
 });
 
-/*
+
 //LOGIN
 router.post("/login", async (req, res) => {
     try {
-        const newUser = new User({
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password,
-
-        })
-
-        const user = await newUser.save();
-        res.status(200).json(user);
+      const user = await User.findOne({ username: req.body.username });
+      !user && res.status(400).json("Wrong credentials!");
+  
+      const validated = await bcrypt.compare(req.body.password, user.password);
+      !validated && res.status(400).json("Wrong credentials!");
+  
+      const { password, ...others } = user._doc;
+      res.status(200).json(others);
     } catch (err) {
       res.status(500).json(err);
     }
-});
-*/
+  });
   
 module.exports = router;
